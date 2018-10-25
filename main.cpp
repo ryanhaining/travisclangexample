67dc980565f9989ec6b829c1a1da036625aaf205
Ryan Haining <haining.cpp@gmail.com>
2018-10-25 03:04:57 -0400

    const string

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   std::variant<int, double> v;
-  std::string s = "hello world";
+  const std::string s = "hello world";
   std::cout << s << '\n';
   std::cout << s.size() << '\n';
   std::cout << s.empty() << '\n';
